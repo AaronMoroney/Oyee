63bb4d0a998ef1e868423230c8e9d132829f0dda
AaronMoroneyDesignCo <102585645+AaronMoroneyDesignCo@users.noreply.github.com>
2022-09-13 21:25:41 +0100

    *frontend* home-feed, proof of concept

---
+++ frontend/src/components/posts/Posts.js
@@ -0,0 +1,68 @@
+import React from 'react'
+//styles
+import '../../styles/components/posts/_posts.scss'
+
+function Posts() {
+
+    
+
+    return (
+    <>
+     <div className='post-feed__parent'>
+     <div className='post-feed__buffer-top'></div>
+        <div className='post-feed__post'>
+            
+        </div>
+        <div className='post-feed__post'>
+            
+        </div>
+        <div className='post-feed__post'>
+            
+        </div>
+        <div className='post-feed__post'>
+            
+        </div>
+        <div className='post-feed__post'>
+            
+        </div>
+        <div className='post-feed__post'>
+            
+        </div>
+        <div className='post-feed__post'>
+            
+        </div>
+        <div className='post-feed__post'>
+            
+        </div>
+        <div className='post-feed__post'>
+            
+        </div>
+        <div className='post-feed__post'>
+            
+        </div>
+        <div className='post-feed__post'>
+            
+        </div>
+        <div className='post-feed__post'>
+            
+        </div>
+        <div className='post-feed__post'>
+            
+        </div>
+        <div className='post-feed__post'>
+            
+        </div>
+        <div className='post-feed__post'>
+            
+        </div>
+        <div className='post-feed__post'>
+            
+        </div>
+        <div className='post-feed__buffer-bottom'></div>
+     </div>
+    </>
+   
+  )
+}
+
+export default Posts
--- frontend/src/components/ui-template/Feed.js
+++ frontend/src/components/ui-template/Feed.js
@@ -1,10 +1,11 @@
+import Posts from '../posts/Posts'
 import React from 'react'
 
 function Feed() {
   return (
     <div className='home-feed__feed'>
       <div>
-
+        <Posts />
       </div>
     </div>
   )
